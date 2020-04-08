bee1b61bf8b78581004a20ad38a00da9555ea144
yeeunlee <chloe.yeeunl@gmail.com>
2020-04-08 19:55:04 +0900

    comment filter

--- comment/urls.py
+++ comment/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CommentView 
+from .views import CommentView, CommentfilterView
 
 urlpatterns = [
         path('', CommentView.as_view()),
+        path('/comment', CommentfilterView.as_view()),
 ]
--- comment/views.py
+++ comment/views.py
@@ -14,9 +14,20 @@ class CommentView(View):
         ).save()
         return HttpResponse(status=200)
 
+class CommentfilterView(View):
     def get(self, request):
-        comment = Comment.objects.values()
-        return JsonResponse({'comment_list':list(comment)}, status=200)
+        data = json.loads(request.body)
+        comment_user = data.get('user', None)
+        try:
+            if Comment.objects.filter(user=comment_id):
+               user_data = Comment.objects.filter(user=comment_id)
+               user_data_length = len(user_data)
+               my_list={}
+               for num in range(user_data_length):
+                   my_list[num] = user_data[num].comment
+            return JsonResponse({'comment_list':my_list}, status=200)
 
+        except TypeError:
+            return JsonResponse({'message': 'INVALID_USER'}, status = 401)
 
 
